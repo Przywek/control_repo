74f500277a55a55dcc7dbe0b4389c3aba253a32b
Przywek <26165332+Przywek@users.noreply.github.com>
2021-01-16 17:05:25 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,8 @@ node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "Welcome to $(fqdn)\n",
+    content => "Welcome to ${fqdn}\n",
+    owner => 'root',
   }
 }
 node /^web/ { 

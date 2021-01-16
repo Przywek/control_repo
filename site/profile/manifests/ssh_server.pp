7432329db2a3fd17524abc15c9547eb00687ab32
Przywek <26165332+Przywek@users.noreply.github.com>
2021-01-16 17:12:05 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => ' AAAAB3NzaC1yc2EAAAADAQABAAABAQDPmj+yHDDEC1vZlyCom95CNG+gAYQRJgsmLhpQcebRbv6d3RCtLMQT/9OiIhbdVt3EZnPqdu2OsYlIEv/sabPJgfX6AiQlwzO2hKZUduHbff6fwFbbR7FPjQg+WV9S8c0Ui4xwntwSeasSLFYJ3xYsNfjlGbni9zojkJOXP3wsfuzD6nfTBa5NmDjhzwLX5VLowiGesxfGD9lrOWmCgRFA+cFdKhglMmgduvzhTcGAppSGRxaduIt12gyVFcKmbneT4le+wAEizk6Z5v+qaTk05g2orAYNZxF2W6NWTg3N7gwqbw0z9Ck3MTQQU3m5rJgGCIgsDrUS8gso8jqh9Qsl',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPmj+yHDDEC1vZlyCom95CNG+gAYQRJgsmLhpQcebRbv6d3RCtLMQT/9OiIhbdVt3EZnPqdu2OsYlIEv/sabPJgfX6AiQlwzO2hKZUduHbff6fwFbbR7FPjQg+WV9S8c0Ui4xwntwSeasSLFYJ3xYsNfjlGbni9zojkJOXP3wsfuzD6nfTBa5NmDjhzwLX5VLowiGesxfGD9lrOWmCgRFA+cFdKhglMmgduvzhTcGAppSGRxaduIt12gyVFcKmbneT4le+wAEizk6Z5v+qaTk05g2orAYNZxF2W6NWTg3N7gwqbw0z9Ck3MTQQU3m5rJgGCIgsDrUS8gso8jqh9Qsl',
   }  
 }
